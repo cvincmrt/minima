e27d06e9c7152f3d43af1654bf6b66f68bcea6f2
cvincmrt <cvincmrt@gmail.com>
2025-12-09 22:18:54 +0100

    Vytvoril som home controler

---
+++ app/controllers/home.php
@@ -0,0 +1,8 @@
+<?php
+
+class Home
+{
+    function index(){
+        echo "home controler a spustila sa metoda index";
+    }
+} 
--- app/core/app.php
+++ app/core/app.php
@@ -2,14 +2,42 @@
 
 class App
 {
+    private $controller = "home";
+    
+    private $method = "index";
+
+    private $params = []; 
+
     public function __construct()
     {
         $url = $this->splitUrl();
-        show($url);
+        //show($url[0]);
+
+        if(file_exists("../app/controllers/".$url[0].".php")){
+            $this->controller = strtolower($url[0]);
+            unset($url[0]);
+            show($url);
+        }
+
+        require "../app/controllers/".$this->controller.".php";
+
+        $this->controller = new $this->controller;
+
+        if(isset($url[1])){
+            if(method_exists($this->controller, $url[1])){
+                $this->method = $url[1]; 
+                unset($url[1]);
+            }
+
+        }
+
+        //spust class a methodu
+         show(array_values($url));
+
     }
 
     private function splitUrl(){
-        return explode("/", trim($_GET["url"], "/"));
+        return explode("/", filter_var(trim($_GET["url"], "/")), FILTER_SANITIZE_URL);
     }
 
 
