4e2088150edb94eb570fb40b5c9253187ec475dc
cvincmrt <cvincmrt@gmail.com>
2025-12-09 22:45:18 +0100

    Pridal som funkciu call_user_func_array

--- app/core/app.php
+++ app/core/app.php
@@ -11,32 +11,39 @@ class App
     public function __construct()
     {
         $url = $this->splitUrl();
-        //show($url[0]);
+        
 
         if(file_exists("../app/controllers/".$url[0].".php")){
             $this->controller = strtolower($url[0]);
             unset($url[0]);
-            show($url);
+           
         }
 
         require "../app/controllers/".$this->controller.".php";
 
+       
         $this->controller = new $this->controller;
 
         if(isset($url[1])){
+           
             if(method_exists($this->controller, $url[1])){
+         
                 $this->method = $url[1]; 
                 unset($url[1]);
             }
 
         }
 
-        //spust class a methodu
-         show(array_values($url));
+        //spust class a methodu 
+         $this->params = array_values($url);
+
+         call_user_func_array([$this->controller,$this->method], $this->params);
+         
 
     }
 
     private function splitUrl(){
+
         return explode("/", filter_var(trim($_GET["url"], "/")), FILTER_SANITIZE_URL);
     }
 
